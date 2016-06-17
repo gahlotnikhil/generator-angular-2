53c8e087944f9533da99157855793931b570c900
gahlotnikhil <gahlotnikhil@gmail.com>
2016-06-17 03:31:31 -0600

    Fixed tests

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,7 +13,6 @@ module.exports = yeoman.Base.extend({
     ));
 
     var prompts = [{
-      //type: 'confirm',
       name: 'projname',
       message: 'What would be your project name?'
     }];
@@ -23,41 +22,41 @@ module.exports = yeoman.Base.extend({
       this.props = props;
       this.projname = _.kebabCase(props.projname);
 
-      //this.log('Project name entered is ' + this.projname);
+      // this.log('Project name entered is ' + this.projname);
     }.bind(this));
   },
 
   writing: function () {
-    
     var projname = this.projname;
     mkdirp(this.projname, function (err) {
-        if (err) console.error(err)
-        else console.log(projname + ' created!')
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(projname + ' created!');
+      }
     });
 
     this.fs.copy(
       this.templatePath('client'),
-      this.destinationPath(this.projname + '/' + 'client')
+      this.destinationPath(this.projname + '/client')
     );
 
     this.fs.copy(
       this.templatePath('app'),
-      this.destinationPath(this.projname + '/' + 'app')
+      this.destinationPath(this.projname + '/app')
     );
 
-    this.template('package.json', this.projname + '/' + 'package.json');
-    this.template('bs-config.json', this.projname + '/' + 'bs-config.json');
-    this.template('index.html', this.projname + '/' + 'index.html');
-    this.template('typings.json', this.projname + '/' + 'typings.json');
-    this.template('tsconfig.json', this.projname + '/' + 'tsconfig.json');
-    this.template('systemjs.config.js', this.projname + '/' + 'systemjs.config.js');
-    
-    this.template('README.md', this.projname + '/' + 'README.md');
-
-    this.template('client/html/component.html', this.projname + '/' + 'client/html/component.html');
+    this.template('package.json', this.projname + '/package.json');
+    this.template('bs-config.json', this.projname + '/bs-config.json');
+    this.template('index.html', this.projname + '/index.html');
+    this.template('typings.json', this.projname + '/typings.json');
+    this.template('tsconfig.json', this.projname + '/tsconfig.json');
+    this.template('systemjs.config.js', this.projname + '/systemjs.config.js');
+    this.template('README.md', this.projname + '/README.md');
+    this.template('client/html/component.html', this.projname + '/client/html/component.html');
   },
 
   install: function () {
-    //this.installDependencies();
+    // this.installDependencies();
   }
 });
--- test/app.js
+++ test/app.js
@@ -4,15 +4,37 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-angular-2:app', function () {
+  var projname = 'test-app';
+
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
+      .withPrompts({projname: projname})
       .toPromise();
   });
 
-  it('creates files', function () {
+  it('creates root files', function () {
     assert.file([
-      'dummyfile.txt'
+      projname + '/package.json',
+      projname + '/bs-config.json',
+      projname + '/index.html',
+      projname + '/typings.json',
+      projname + '/tsconfig.json',
+      projname + '/systemjs.config.js',
+      projname + '/README.md'
+    ]);
+  });
+
+  it('creates client directory', function () {
+    assert.file([
+      projname + '/client/css/app.css',
+      projname + '/client/html/component.html'
+    ]);
+  });
+
+  it('creates app directory', function () {
+    assert.file([
+      projname + '/app/app.ts',
+      projname + '/app/js/component.ts'
     ]);
   });
 });
